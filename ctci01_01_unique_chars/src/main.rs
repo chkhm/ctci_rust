8417139050a353be70755ec21bd0d007dc2a213c
Christoph Kuhmuench <christoph.kuhmuench@siemens.com>
2025-04-03 09:27:33 -0400

    Added a test

--- ctci01_01_unique_chars/src/main.rs
+++ ctci01_01_unique_chars/src/main.rs
@@ -1,6 +1,17 @@
 
 use std::{collections::HashSet, env};
 
+fn is_unique_brute(s : &str) -> bool {
+    for i in 0..s.len() {
+        for j in i+1..s.len() {
+            if s.as_bytes()[i] == s.as_bytes()[j] {
+                return false;
+            }
+        }
+    }
+    true
+}
+
 fn is_unique_set(s : &str) -> bool {
     let mut characters = HashSet::new();
     for c in s.chars() {
@@ -33,8 +44,27 @@ fn is_unique_sort(s : &mut str) -> bool {
 fn main() {
     let mut args : Vec<String> = env::args().collect();
     for i in 1..args.len() {
+        let is_unique0 = is_unique_brute(&args[i]);
         let is_unique1 = is_unique_set(&args[i]);
         let is_unique2 = is_unique_sort(&mut args[i]);
-        println!("{}. Input string \"{}\" \t is_unique (use set): {} \t is_unique (sort): {}", i, args[i], is_unique1, is_unique2);
+        println!("{}. Input string \"{}\" \t is_unique (brute): {} \t is_unique (use set): {} \t is_unique (sort): {}", 
+            i, args[i], is_unique0, is_unique1, is_unique2);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_simple_unique() {
+        let mut args  = vec![("abcxyz".to_string(), true), ("axayat".to_string(), false)];
+
+        for i in 0..args.len() {
+            let is_unique0 = is_unique_brute(&args[i].0);
+            let is_unique1 = is_unique_set(&args[i].0);
+            let is_unique2 = is_unique_sort(&mut args[i].0);
+            assert!(is_unique0 == args[i].1 && is_unique1 == args[i].1 && is_unique2 == args[i].1);
+        }
     }
 }
