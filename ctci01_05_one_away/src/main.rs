7e9695b7960689c5afa209c1964e28f5919aa8c7
Christoph Kuhmuench <kuhmuench@email.de>
2025-04-05 17:24:51 -0400

    Improve One Away implementation: - Add support for multiple string pair comparisons - Improve documentation and error handling - Add better formatted output with pair numbering - Maintain backward compatibility with default values

--- ctci01_05_one_away/src/main.rs
+++ ctci01_05_one_away/src/main.rs
@@ -1,97 +1,162 @@
-
 use std::env;
 
-fn one_away(s1 : &str, s2 : &str) -> bool {
-    
+/// Determines if two strings are one edit away from each other.
+/// 
+/// An edit can be one of three operations:
+/// - Insert a character
+/// - Remove a character
+/// - Replace a character
+/// 
+/// # Arguments
+/// * `s1` - First string to compare
+/// * `s2` - Second string to compare
+/// 
+/// # Returns
+/// `true` if strings are one or zero edits away from each other, `false` otherwise
+fn one_away(s1: &str, s2: &str) -> bool {
+    // Check if length difference is more than 1
     let l1 = s1.len();
     let l2 = s2.len();
     let d = usize::abs_diff(l1, l2);
     if d > 1 {
         return false;
     }
+    
+    // Equal length strings - need to check for replacements
     if l1 == l2 {
-        return replacement_iter(s1, s2);
+        return replacement_check(s1, s2);
     }
+    
+    // Different length strings - need to check for insertions/deletions
     if l1 > l2 {
-        return remove_iter(s1, s2);
-    }
-    else {
-        return remove_iter(s2, s1);
+        return insertion_check(s1, s2);
+    } else {
+        return insertion_check(s2, s1);
     }
 }
 
-// we know s1 and s2 are of the same length
-fn replacement_iter(s1 : &str, s2 : &str) -> bool {
-    // first we clone the underlying string so that we can edit
-    let a1 = s1.to_owned();
-    let a2 = s2.to_owned();
-
-    let mut edit_done = false;
-    // crazy iterator shit for rust... not sure if this language is really as cool as they say
-    // it makes you write a lot of ugly code
-    let it1 = a1.chars().into_iter();
-    let it2 = a2.chars().into_iter();
-
-    for mut it in it1.zip(it2) {
-        if it.0 != it.1 && edit_done {
-            return false;
-        }
-        if it.0 != it.1 {
-            it.0 = it.1;
-            edit_done = true;
+/// Checks if two strings of the same length are one character replacement away.
+/// 
+/// # Arguments
+/// * `s1` - First string to compare
+/// * `s2` - Second string to compare
+/// 
+/// # Returns
+/// `true` if strings are at most one replacement away, `false` otherwise
+fn replacement_check(s1: &str, s2: &str) -> bool {
+    let mut differences = 0;
+    
+    // Zip iterators to compare characters at the same positions
+    for (c1, c2) in s1.chars().zip(s2.chars()) {
+        if c1 != c2 {
+            differences += 1;
+            
+            // More than one difference means more than one edit
+            if differences > 1 {
+                return false;
+            }
         }
     }
+    
     true
 }
 
-// we can safely assume that s1 is exactly one char longer than s2
-fn remove_iter(s1 : &str, s2 : &str) -> bool {
-    // first we clone the underlying string so that we can edit
-    let a1 = s1.to_owned();
-    let a2 = s2.to_owned();
-
-    let mut edit_done = false;
-    // crazy iterator shit for rust... not sure if this language is really as cool as they say
-    // it makes you write a lot of ugly code
-    let mut it1 = a1.chars().into_iter();
-    let mut it2 = a2.chars().into_iter();
-
-    let mut c1 = it1.next();
-    let mut c2 = it2.next();
-    while c2 != None {
-        if c1.unwrap() != c2.unwrap() && edit_done {
-            return false;
-        }
-        if c1.unwrap() != c2.unwrap() {
-            c1 = it1.next();
-            edit_done = true;
-        }
-        else {
-            c1 = it1.next();
-            c2 = it2.next();
+/// Checks if two strings are one character insertion/deletion away.
+/// 
+/// This function assumes that `s1` is exactly one character longer than `s2`.
+/// 
+/// # Arguments
+/// * `s1` - Longer string (exactly one character longer than s2)
+/// * `s2` - Shorter string
+/// 
+/// # Returns
+/// `true` if removing one character from s1 can make it equal to s2, `false` otherwise
+fn insertion_check(s1: &str, s2: &str) -> bool {
+    let mut it1 = s1.chars().peekable();
+    let mut it2 = s2.chars().peekable();
+    let mut skipped = false;
+    
+    // Compare characters until we reach the end of the shorter string
+    while let Some(&c2) = it2.peek() {
+        match it1.peek() {
+            Some(&c1) if c1 == c2 => {
+                // Characters match, advance both iterators
+                it1.next();
+                it2.next();
+            },
+            Some(_) => {
+                // Characters don't match
+                if skipped {
+                    // Already skipped one character, this would be a second mismatch
+                    return false;
+                }
+                
+                // Skip the extra character in the longer string
+                it1.next();
+                skipped = true;
+            },
+            None => {
+                // Reached the end of s1 before s2, which shouldn't happen
+                return false;
+            }
         }
     }
+    
+    // If we've made it through the entire shorter string, we're good
     true
 }
 
+/// Main function to run the One Away algorithm with command-line arguments.
+///
+/// Usage:
+///   cargo run [string1 string2 string3 string4 ...]
+///
+/// Strings are processed in pairs. For example:
+///   - string1 is compared with string2
+///   - string3 is compared with string4
+///   - ...
+///
+/// If an odd number of arguments is provided, the last string is compared 
+/// with a default value. If no arguments are provided, default strings are used.
 fn main() {
-    let args : Vec<String> = env::args().collect();
-
-    let mut s1 = "abcdefg";
-    let mut s2 = "axxdefg";
-    //let mut s1 = "";
-    //let mut s2 = "";
-
-    if args.len() > 2 {
-        s2 = &args[2];
-        s1 = &args[1];
+    // Skip program name and collect remaining args
+    let args: Vec<String> = env::args().skip(1).collect();
+    
+    // Default values
+    let default_s1 = "abcdefg";
+    let default_s2 = "axxdefg";
+    
+    // Check if no arguments provided
+    if args.is_empty() {
+        let result = one_away(default_s1, default_s2);
+        println!("Are \"{}\" and \"{}\" one edit away from each other? {}", 
+                 default_s1, default_s2, result);
+        return;
     }
-    else if args.len() > 1 {
-        s2 = &args[1];
+    
+    // Process arguments in pairs
+    let mut pair_count = 0;
+    
+    for i in (0..args.len()).step_by(2) {
+        pair_count += 1;
+        let s1 = &args[i];
+        
+        // If this is the last argument and we have an odd number of args
+        let s2 = if i + 1 < args.len() {
+            &args[i + 1]
+        } else {
+            default_s2  // Use default for odd argument
+        };
+        
+        let result = one_away(s1, s2);
+        
+        // Print result with formatting
+        if args.len() > 2 {
+            println!("\nPair {}: ", pair_count);
+        }
+        println!("Are \"{}\" and \"{}\" one edit away from each other? {}", 
+                 s1, s2, result);
     }
-
-    let result = one_away(s1, s2);
-    println!("Distance of \"{}\" and \"{}\" is a one_away: {}", s1, s2, result);
 }
 
 #[cfg(test)]
@@ -106,7 +171,8 @@ mod tests {
             ("abcd", "abc", true),
             ("axbc", "abc", true),
             ("axc", "abc", true),
-            ("abcde", "abc", false)
+            ("abcde", "abc", false),
+            ("", "a", true)
         ];
 
         for x in test_data {
